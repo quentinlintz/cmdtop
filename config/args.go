2d3fca3735628f3137b721ee0897df100b8425b2
Quentin Lintz <quentinlintz@outlook.com>
2024-06-18 19:39:13 -0400

    Add GitHub workflow

--- config/args.go
+++ config/args.go
@@ -8,11 +8,14 @@ import (
 	"strconv"
 )
 
-var usage = `Usage: %s [options]
+var (
+	version = "<unknown>"
+	usage   = `Usage: %s [options]
 Print top commands used from your shell history
 
 Options:
 `
+)
 
 type Config struct {
 	ShellType   string
@@ -52,13 +55,22 @@ func Load() (Config, error) {
 	var config Config
 	config.TopN = 5
 
+	var showVersion bool
+
 	flag.Usage = func() {
 		name := path.Base(os.Args[0])
 		fmt.Fprintf(os.Stderr, usage, name)
 		flag.PrintDefaults()
 	}
 	flag.Var(&Top{&config.TopN}, "top", "how many top commands to return")
+	flag.BoolVar(&showVersion, "version", false, "print version and exit")
 	flag.Parse()
+
+	if showVersion {
+		name := path.Base(os.Args[0])
+		fmt.Printf("%s version %s\n", name, version)
+		os.Exit(0)
+	}
 
 	if err := parseEnv(&config); err != nil {
 		return config, fmt.Errorf("failed to parse environment variables")
