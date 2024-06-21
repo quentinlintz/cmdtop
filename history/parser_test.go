3561ef9910c23bfcd22ebe61cb91ac928aba6a91
Quentin Lintz <quentinlintz@outlook.com>
2024-06-20 20:32:15 -0400

    Sort commands for better comparison, refactor test into table driven test

--- history/parser_test.go
+++ history/parser_test.go
@@ -2,6 +2,7 @@ package history
 
 import (
 	"path/filepath"
+	"sort"
 	"testing"
 
 	"github.com/quentinlintz/cmdtop/models"
@@ -13,18 +14,39 @@ var expectedCommands = []models.Command{
 	{Name: "git", Count: 2},
 }
 
-func TestZshParseHistoryContents(t *testing.T) {
-	testFile := filepath.Join("testdata", "zsh_history")
-	parser := &ZshParser{}
-	testParser(t, parser, testFile, expectedCommands)
+func TestParseHistory(t *testing.T) {
+	tests := []struct {
+		name        string
+		parser      Parser
+		historyFile string
+	}{
+		{"Zsh", &ZshParser{}, "zsh_history"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			testFile := filepath.Join("testdata", tt.historyFile)
+			testParser(t, tt.parser, testFile, expectedCommands)
+		})
+	}
 }
 
-func TestZshParseHistoryFileNotFound(t *testing.T) {
-	testFile := filepath.Join("testdata", "not_found")
-	parser := &ZshParser{}
-	_, err := parser.ParseHistory(testFile)
-	if err == nil {
-		t.Fatal("Expected error when parsing non-existent file, but got none")
+func TestHistoryFileNotFound(t *testing.T) {
+	tests := []struct {
+		name   string
+		parser Parser
+	}{
+		{"Zsh", &ZshParser{}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			testFile := filepath.Join("testdata", "not_found")
+			_, err := tt.parser.ParseHistory(testFile)
+			if err == nil {
+				t.Fatal("Expected error when parsing non-existent file, but got none")
+			}
+		})
 	}
 }
 
@@ -37,6 +59,14 @@ func testParser(t *testing.T, parser Parser, testFile string, expected []models.
 	if len(commands) != len(expected) {
 		t.Fatalf("Expected %d commands, got %d", len(expected), len(commands))
 	}
+
+	sort.Slice(commands, func(i, j int) bool {
+		return commands[i].Name < commands[j].Name
+	})
+
+	sort.Slice(expected, func(i, j int) bool {
+		return expected[i].Name < expected[j].Name
+	})
 
 	for i, cmd := range commands {
 		if cmd.Name != expected[i].Name || cmd.Count != expected[i].Count {
